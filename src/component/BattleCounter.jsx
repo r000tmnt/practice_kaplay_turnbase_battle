d0ebbd1a5d0513741bf4fa1456a3abcd2a9e156e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-05-21 10:44:45 +0800

    [Create] Battle counter component

---
+++ src/component/BattleCounter.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux"
+
+export default function BattleCounter() {
+    const wave = useSelector(state => state.game.wave)
+    const turn = useSelector(state => state.game.turn)
+    const tension = useSelector(state => state.game.tension)
+
+    return (
+        <div className="counter flex">
+            <div>
+                <div className='wave'>
+                    WAVE { wave.current }/{ wave.max }
+                </div>
+                <div className='turn'>
+                    Turn { turn }
+                </div>                
+            </div>
+            <div>
+                <div className="tension">
+                    Tension { tension.current }/{ tension.max }
+                </div>
+            </div>
+        </div>
+
+    )
+}
