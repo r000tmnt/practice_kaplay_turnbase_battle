1a71562b544a7a7b9399b7b9c7586926ac5784ab
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-05-26 16:04:12 +0800

    [Update] Set counter's left position

--- src/component/BattleCounter.jsx
+++ src/component/BattleCounter.jsx
@@ -4,9 +4,11 @@ export default function BattleCounter() {
     const wave = useSelector(state => state.game.wave)
     const turn = useSelector(state => state.game.turn)
     const tension = useSelector(state => state.game.tension)
+    const gameWidth = useSelector(state => state.setting.width)
+    // const scale = useSelector(state => state.setting.scale)
 
     return (
-        <div className="counter flex ui">
+        <div className="counter flex ui" style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }}>
             <div>
                 <div className='wave'>
                     WAVE { wave.current }/{ wave.max }
