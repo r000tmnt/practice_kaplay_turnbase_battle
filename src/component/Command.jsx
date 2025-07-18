a8e034f0eb99a68b4ae743abedd28b5f9056da90
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-07-18 16:35:13 +0800

    [Update] Set value with localStorage to check if player is changing position. Set the next loop in callback if the value is false.

--- src/component/Command.jsx
+++ src/component/Command.jsx
@@ -6,7 +6,7 @@ import k from '../lib/kaplay'
 import store from "../store/store"
 import { 
   updateUnit, setPointedTarget, setCurrentActivePlayer, 
-  setAllToStop, setActiveUnits 
+  setAllToStop, setActiveUnits
 } from '../store/game'
 import { 
   controller, attack, castSkill, 
@@ -20,7 +20,8 @@ import {
 
 const { 
   onClick, onHover, tween, 
-  vec2, easings
+  vec2, easings, setData, 
+  getData
 } = k
 
 export default function Command() {
@@ -184,7 +185,10 @@ export default function Command() {
               currentActivePlayer
             ) 
           },
-          callback: () => { loopConstructor(currentActivePlayer, unit, positionRef, null, null) }
+          callback: () => {
+            store.dispatch(updateUnit({name: unit.name, attribute: unit.attribute, action: ''})) 
+            loopConstructor(currentActivePlayer, unit, positionRef, null, null) 
+          }
         }
         const activeUnits = store.getState().game.activeUnits
         dispatch(
@@ -206,9 +210,8 @@ export default function Command() {
           },
           callback: null
         }
-        // Pause timers of the other units
-        // pauseOrResume({ index: currentActivePlayer, value: true })
-        // actionClear(unit, currentActivePlayer)
+
+        setData('changing', true)
         const activeUnits = store.getState().game.activeUnits
         // Only enemy is allow to act in the next few seconds
         dispatch(
@@ -249,12 +252,13 @@ export default function Command() {
   }
 
   const spriteClickEvent = onClick('unit', (sprite) => {
+    const currentActivePlayer = store.getState().game.currentActivePlayer
     if(currentActivePlayer < 0) {
       console.log('current active player error', currentActivePlayer)
       return
     }
 
-    const unit = JSON.parse(JSON.stringify(units[currentActivePlayer]))
+    const unit = JSON.parse(JSON.stringify(store.getState().game.units[currentActivePlayer]))
     
     // Get the index of the clicked sprite
     const tag = sprite.tags.find((tag) => tag.includes('index_'));
@@ -267,7 +271,10 @@ export default function Command() {
 
     const input = {
       action: () => {},
-      callback: () => { loopConstructor(currentActivePlayer, unit, positionRef, null, null) }
+      callback: () => { 
+        const changing = getData('changing', false)
+        if(!changing) loopConstructor(currentActivePlayer, unit, positionRef, null, null) 
+      }
     }
 
     switch(unit.action){
@@ -375,123 +382,128 @@ export default function Command() {
   }, [activeUnits])  
   // endregion    
 
-    useEffect(() => {
-      console.log('currentActivePlayer update ', currentActivePlayer)
-      if(currentActivePlayer < 0){
-        setShowCancel(false)
-        setSkillList([])
+  useEffect(() => {
+    console.log('currentActivePlayer update ', currentActivePlayer)
+    if(currentActivePlayer < 0){
+      setShowCancel(false)
+      setSkillList([])
+    }
+  }, [currentActivePlayer])
+
+  useEffect(() => {
+    console.log('units updated', units)
+  }, [units])
+
+  return(
+    <>
+      {/* <div>{units[currentActivePlayer]? units[currentActivePlayer].name : 'null'}</div> */}
+      <div className={`command ui ${currentActivePlayer >= 0 && units[currentActivePlayer] !== undefined && !units[currentActivePlayer].action.length? 'show' : 'hide'}`} style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }} >
+        <div className='avatar'>
+          { currentActivePlayer >= 0? units[currentActivePlayer]?.name : '' }
+          <img src="battle/Animations/Defensive_Stance.png" alt="player" style={{ width: `${gameWidth * 0.2}px`, height: `${gameWidth * 0.2}px`, objectFit: 'cover' }}></img>
+          <div className='meter'>
+            <label>
+              HP
+              <div className='bar hp' style={{ width: `${(units[currentActivePlayer]?.attribute.hp / units[currentActivePlayer]?.attribute.maxHp) * 100}%` }}>
+                { units[currentActivePlayer]?.attribute.hp }/{ units[currentActivePlayer]?.attribute.maxHp }
+              </div>
+            </label>
+            <label>
+              MP
+              <div className='bar mp' style={{ width: `${(units[currentActivePlayer]?.attribute.mp / units[currentActivePlayer]?.attribute.maxMp) * 100}%` }}>
+                { units[currentActivePlayer]?.attribute.mp }/{ units[currentActivePlayer]?.attribute.maxMp }
+              </div>
+            </label>
+          </div>
+        </div>
+        <div className='action'>
+          <div className='relative'>
+            <button className='position-center' onClick={() => setAction('attack')}>ATTACK</button>
+          </div>
+          <div className='relative'>
+            <button className='position-center' onClick={() => setAction('skill')}>SKILL</button>
+          </div>
+          <div className='relative'>
+            <button className='position-center' onClick={() => setAction('item')}>ITEM</button>
+          </div>
+          <div className='relative'>
+            <button className='position-center' onClick={() => setAction('defense')}>DEFENSE</button>
+          </div>
+          <div className='relative'>
+            <button className='position-center' onClick={() => setAction('change')}>CHANGE</button>
+          </div>
+          <div className='relative'>
+            <button className='position-center' onClick={() => setAction('escape')}>ESCAPE</button>
+          </div>
+        </div>
+      </div>
+
+      {
+        // Skill menu 
       }
-    }, [currentActivePlayer])
-
-    return(
-      <>
-        <div className={`command ui ${currentActivePlayer >= 0 && units[currentActivePlayer] !== undefined && !units[currentActivePlayer].action.length? 'show' : 'hide'}`} style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }} >
-          <div className='avatar'>
-            { currentActivePlayer >= 0? units[currentActivePlayer]?.name : '' }
-            <img src="battle/Animations/Defensive_Stance.png" alt="player" style={{ width: `${gameWidth * 0.2}px`, height: `${gameWidth * 0.2}px`, objectFit: 'cover' }}></img>
-            <div className='meter'>
-              <label>
-                HP
-                <div className='bar hp' style={{ width: `${(units[currentActivePlayer]?.attribute.hp / units[currentActivePlayer]?.attribute.maxHp) * 100}%` }}>
-                  { units[currentActivePlayer]?.attribute.hp }/{ units[currentActivePlayer]?.attribute.maxHp }
-                </div>
-              </label>
-              <label>
-                MP
-                <div className='bar mp' style={{ width: `${(units[currentActivePlayer]?.attribute.mp / units[currentActivePlayer]?.attribute.maxMp) * 100}%` }}>
-                  { units[currentActivePlayer]?.attribute.mp }/{ units[currentActivePlayer]?.attribute.maxMp }
-                </div>
-              </label>
-            </div>
-          </div>
-          <div className='action'>
-            <div className='relative'>
-              <button className='position-center' onClick={() => setAction('attack')}>ATTACK</button>
-            </div>
-            <div className='relative'>
-              <button className='position-center' onClick={() => setAction('skill')}>SKILL</button>
-            </div>
-            <div className='relative'>
-              <button className='position-center' onClick={() => setAction('item')}>ITEM</button>
-            </div>
-            <div className='relative'>
-              <button className='position-center' onClick={() => setAction('defense')}>DEFENSE</button>
-            </div>
-            <div className='relative'>
-              <button className='position-center' onClick={() => setAction('change')}>CHANGE</button>
-            </div>
-            <div className='relative'>
-              <button className='position-center' onClick={() => setAction('escape')}>ESCAPE</button>
-            </div>
-          </div>
-        </div>
-
-        {
-          // Skill menu 
-        }
-        <div className={`skill-list ui ${skillList.length > 0? 'show' : 'hide'}`} style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }}>
-          <div className="action">
-            { skillList.map((s, index) => {
-                if(s)
-                  return (
-                    <button key={index} className={`skill-item ${units[currentActivePlayer] !== undefined && units[currentActivePlayer].attribute.mp < s.cost['mp']? 'not-enough' : ''}`} 
-                    onClick={() => {
-                      setShowCancel(true)
-                      playerAction('skill', units[currentActivePlayer], s)
-                      setSkillList([])
-                    }}>
-                      {/* <img src={s.icon} alt={s.name} /> */}
-                      <div className='skill-name'>{s.name}</div>
-                      <div className='skill-cost'>{
-                        Object.keys(s.cost).map((key, i) => {
-                          return (
-                            <span key={i}>
-                              {key}: {s.cost[key]} 
-                            </span>
-                          )
-                        })  
-                      }</div>
-                    </button>
-                  )
-                }) }
-          </div>
-          <button className="back" style={{ width: '100%' }}
-          onClick={() => {
-            dispatch(updateUnit({ name: units[currentActivePlayer].name, attribute: units[currentActivePlayer].attribute, action: '' }))
-            setSkillList([])
-          }}>BACK</button>
-        </div>
-
-        {
-          // Item menu
-        }
-        <div className={`item-list ui ${itemList.length > 0? 'show' : 'hide'}`} style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }}>
-          <div className="action">
-            { itemList.map((item, index) => {
-                if(item)
-                  return (
-                    <button key={index} className={`item`} onClick={() => {
-                      setShowCancel(true)
-                      playerAction('item', units[currentActivePlayer], item)
-                      setItemList([])
-                    }}>
-                      <div className='skill-name'>{item.name}</div>
-                      <div className='skill-cost'>{item.amount}</div>
-                    </button>
-                  )
+      <div className={`skill-list ui ${skillList.length > 0? 'show' : 'hide'}`} style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }}>
+        <div className="action">
+          { skillList.map((s, index) => {
+              if(s)
+                return (
+                  <button key={index} className={`skill-item ${units[currentActivePlayer] !== undefined && units[currentActivePlayer].attribute.mp < s.cost['mp']? 'not-enough' : ''}`} 
+                  onClick={() => {
+                    setShowCancel(true)
+                    playerAction('skill', units[currentActivePlayer], s)
+                    setSkillList([])
+                  }}>
+                    {/* <img src={s.icon} alt={s.name} /> */}
+                    <div className='skill-name'>{s.name}</div>
+                    <div className='skill-cost'>{
+                      Object.keys(s.cost).map((key, i) => {
+                        return (
+                          <span key={i}>
+                            {key}: {s.cost[key]} 
+                          </span>
+                        )
+                      })  
+                    }</div>
+                  </button>
+                )
               }) }
-          </div>
-          <button className="back" style={{ width: '100%' }}
-          onClick={() => {
-            dispatch(updateUnit({ name: units[currentActivePlayer].name, attribute: units[currentActivePlayer].attribute, action: '' }))
-            setItemList([])
-          }}>BACK</button>
         </div>
+        <button className="back" style={{ width: '100%' }}
+        onClick={() => {
+          dispatch(updateUnit({ name: units[currentActivePlayer].name, attribute: units[currentActivePlayer].attribute, action: '' }))
+          setSkillList([])
+        }}>BACK</button>
+      </div>
 
-        <button 
-          className={`back ui ${currentActivePlayer >= 0 && showCancel? 'show' : 'hide'}`} 
-          style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }}
-          onClick={() => cancelAction()}>BACK</button>      
-      </>
-    )
+      {
+        // Item menu
+      }
+      <div className={`item-list ui ${itemList.length > 0? 'show' : 'hide'}`} style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }}>
+        <div className="action">
+          { itemList.map((item, index) => {
+              if(item)
+                return (
+                  <button key={index} className={`item`} onClick={() => {
+                    setShowCancel(true)
+                    playerAction('item', units[currentActivePlayer], item)
+                    setItemList([])
+                  }}>
+                    <div className='skill-name'>{item.name}</div>
+                    <div className='skill-cost'>{item.amount}</div>
+                  </button>
+                )
+            }) }
+        </div>
+        <button className="back" style={{ width: '100%' }}
+        onClick={() => {
+          dispatch(updateUnit({ name: units[currentActivePlayer].name, attribute: units[currentActivePlayer].attribute, action: '' }))
+          setItemList([])
+        }}>BACK</button>
+      </div>
+
+      <button 
+        className={`back ui ${currentActivePlayer >= 0 && showCancel? 'show' : 'hide'}`} 
+        style={{ left: `${(window.innerWidth - gameWidth) / 2}px` }}
+        onClick={() => cancelAction()}>BACK</button>      
+    </>
+  )
 }
