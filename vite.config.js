56d450f45b1b8fa64f81cc8bcda92171d45fb470
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-08-07 10:21:48 +0800

    [Update] Code splitting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-redux", "@reduxjs/toolkit"], // Separate React into its own chunk
+          kaplay: ["kaplay", "kaplay-atb-plugin"],
+        },
+      },
+    },
+  },  
 })
